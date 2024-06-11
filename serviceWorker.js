7921f874bb967bff2269affd7066b8c1078ded95
hinacoppy <kawakami@js4.so-net.ne.jp>
2024-06-11 18:11:35 +0900

    update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -2,7 +2,7 @@
 // (参考) https://developer.mozilla.org/ja/docs/Web/Progressive_web_apps/Offline_Service_workers
 'use strict';
 
-const cacheName = 'OtokogiGammon-v20230519';
+const cacheName = 'OtokogiGammon-v20240611';
 const ORIGIN = (location.hostname == 'localhost') ? '' : location.protocol + '//' + location.hostname;
 
 const contentToCache = [
@@ -16,14 +16,14 @@ const contentToCache = [
   ORIGIN + '/OtokogiGammon/icon/android-chrome-512x512.png',
   ORIGIN + '/OtokogiGammon/css/OtokogiGame.css',
   ORIGIN + '/OtokogiGammon/css/OtokogiBoard.css',
-  ORIGIN + '/css/font-awesome-animation.min.css',
-  ORIGIN + '/js/fontawesome-all.min.js',
-  ORIGIN + '/js/jquery-3.6.1.min.js',
-  ORIGIN + '/js/inobounce.min.js',
   ORIGIN + '/OtokogiGammon/js/Ogid_class.js',
   ORIGIN + '/OtokogiGammon/js/OtokogiChequer_class.js',
   ORIGIN + '/OtokogiGammon/js/OtokogiBoard_class.js',
-  ORIGIN + '/OtokogiGammon/js/OtokogiGame_class.js'
+  ORIGIN + '/OtokogiGammon/js/OtokogiGame_class.js',
+  ORIGIN + '/css/font-awesome-animation.min.css',
+  ORIGIN + '/js/fontawesome-inuse.min.js',
+  ORIGIN + '/js/jquery-3.7.1.min.js',
+  ORIGIN + '/js/inobounce.min.js',
 ];
 
 self.addEventListener('install', (e) => {
@@ -32,13 +32,16 @@ self.addEventListener('install', (e) => {
       return cache.addAll(contentToCache);
     })
   );
+  self.skipWaiting();
 });
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((r) => {
       return r || fetch(e.request).then((response) => {
         return caches.open(cacheName).then((cache) => {
-          cache.put(e.request, response.clone());
+          if (e.request.url.startsWith('http')) { //ignore chrome-extention: request (refuse error msg)
+            cache.put(e.request, response.clone());
+          }
           return response;
         });
       });
